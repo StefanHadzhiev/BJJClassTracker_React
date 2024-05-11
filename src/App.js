a428f8d0432993bebdb2d419afd620f57607531c
Stefan Hadzhiev <stefanhadzhiev24@gmail.com>
2024-05-11 21:19:48 +0300

    Include bootstrap and bootstrap-react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+
 function App() {
-  return <h3>Initial commit</h3>;
+  return (
+    <div>
+      <Welcome></Welcome>
+      <Form.Control className="row-md-12" type="date" />
+    </div>
+  );
+}
+
+function Welcome() {
+  return <h3>Welcome to the BJJ Class Tracker!</h3>;
 }
 
 export default App;
