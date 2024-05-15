9a1b8a89a294dec4f9909627944d1627418b2819
StefanHadzhiev <stefanhadzhiev24@gmail.com>
2024-05-15 20:51:02 +0300

    Add Class functionality not full

--- src/App.js
+++ src/App.js
@@ -6,32 +6,33 @@ import { useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import moment from "moment";
 
-// https://refine.dev/blog/react-date-picker/#select-range-within-one-component
+// TO DO: FIX ADD CLASS AS IT FILLS THE ARRAY IN AN INCORRECT WAY
+// TO DO : FIX DATE FORMATTING ON ADD NEW CLASS
 
 const initialClasses = [
   {
-    id: 118836,
+    id: crypto.randomUUID(),
     name: "De-La Riva, Spider Guard Pass",
     description:
       "In todays workout we trained a delariva spider guard pass, where the person trying to pass the guard catches the spider guard leg and moves in its direction. He cleans the leg with his adjacent leg and proceeds to put the opponent's leg between his two legs, squeezing. Grips are changed to hip and collar grip. Then a backstep is performed, where the head of the attacker is placed on the belly of the opponent and the attacker's outer knee touches the ground. Then the hip touches the ground as well, while pulling with both hands. A hip escape is performed to get the legs out. Then the attacker advances to side control.",
     date: new Date(2024, 4, 13),
   },
   {
-    id: 118837,
+    id: crypto.randomUUID(),
     name: "Some other workout",
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.",
     date: new Date(2024, 4, 13),
   },
   {
-    id: 118838,
+    id: crypto.randomUUID(),
     name: "No gi workout",
     description:
       "Gi and no Gi are the two forms of Brazilian Jiu-Jitsu. Gi Jiu-Jitsu is grappling with the use of a traditional Gi, which allows you to grab the clothing of your opponent. No Gi is grappling without the traditional uniform, instead you wear shorts and a rash guard. In no Gi you cannot grab your opponent's clothing",
     date: new Date(2024, 4, 14),
   },
   {
-    id: 118839,
+    id: crypto.randomUUID(),
     name: "Gi workout",
     description:
       "BJJ has traditionally been performed and practiced in the gi the heavy martial arts uniform also known as a kimono",
@@ -40,20 +41,18 @@ const initialClasses = [
 ];
 
 function App() {
-  debugger;
   const [date, setDate] = useState(new Date());
   const [showWorkout, setShowWorkout] = useState(false);
   const [bjjClasses, setbjjClasses] = useState(initialClasses);
   const [selctedBjjClass, setselctedBjjClass] = useState(null);
+  const [showAddClass, setShowAddClass] = useState(false);
 
   function handleSetDate(date) {
-    debugger;
     setDate(date.toLocaleDateString());
     setShowWorkout(true);
     setselctedBjjClass(null);
 
     bjjClasses.map((bjjClass) => {
-      debugger;
       if (
         bjjClass.date.getDay() === date.getDay() &&
         bjjClass.date.getMonth() === date.getMonth() &&
@@ -62,9 +61,17 @@ function App() {
         setselctedBjjClass(bjjClass);
       }
     });
+  }
 
-    console.log(date);
-    console.log(date.getDay());
+  function handleOnButtonClick() {
+    setShowAddClass(!showAddClass);
+  }
+
+  function handleAddClass(newClass) {
+    setbjjClasses((bjjClasses) => [...bjjClasses, newClass]);
+    setShowAddClass(false);
+
+    console.log(bjjClasses);
   }
 
   return (
@@ -75,16 +82,27 @@ function App() {
         onChange={(date) => handleSetDate(date)}
       ></DatePicker>
 
-      <Button className="button">Add Person</Button>
-      <Button className="button">Add Class</Button>
-
       {selctedBjjClass ? (
-        <BjjClasses
+        <BjjClass
           showWorkout={showWorkout}
           selctedBjjClass={selctedBjjClass}
-        ></BjjClasses>
+        ></BjjClass>
       ) : (
         <p>No BJJ Class available for this date.</p>
+      )}
+
+      <AddPerson></AddPerson>
+
+      <Button onClick={handleOnButtonClick} className="button">
+        {showAddClass ? "Close" : "Add Class"}
+      </Button>
+      {showAddClass && (
+        <AddClass
+          onAddClass={handleAddClass}
+          onButtonClick={handleOnButtonClick}
+        >
+          {showAddClass}
+        </AddClass>
       )}
     </div>
   );
@@ -94,7 +112,7 @@ function Welcome() {
   return <h3>Welcome to the BJJ Class Tracker!</h3>;
 }
 
-function BjjClasses({ showWorkout, selctedBjjClass }) {
+function BjjClass({ showWorkout, selctedBjjClass }) {
   return (
     <Accordion disabled={showWorkout} defaultActiveKey="0">
       {
@@ -106,6 +124,68 @@ function BjjClasses({ showWorkout, selctedBjjClass }) {
         </Accordion.Item>
       }
     </Accordion>
+  );
+}
+
+function AddClass({ onAddClass }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState(null);
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+
+    console.log(e);
+    const id = crypto.randomUUID();
+
+    const newClass = {
+      id,
+      title,
+      description,
+      date: new Date(date),
+    };
+
+    onAddClass(newClass);
+
+    setTitle("");
+    console.log(e.target);
+  }
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleOnSubmit}>
+        <label>BJJ Class Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+
+        <label>BJJ Class Description:</label>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></input>
+
+        <label>BJJ Class Date:</label>
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        ></input>
+
+        <Button onClick={handleOnSubmit}>Add</Button>
+      </form>
+    </div>
+  );
+}
+
+function AddPerson() {
+  return (
+    <div>
+      <Button className="button">Add Person</Button>
+    </div>
   );
 }
 
